e314cd8182cf4dcd9baf2af83c153845873e7d1c
ahmednasser <ahmednasser.ce@gmail.com>
2020-12-30 06:01:54 +0200

    zz

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const KEY ="337f9e15f531b8e09518f9dcffe668c2";
 
 export const fetchdata=(city)=>async dispatch=>{
-     const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${KEY}`)
+     const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${KEY}`)
     dispatch({
         type:'FETCH-DATA',
         payload:response.data
